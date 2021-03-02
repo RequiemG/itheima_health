be8e2ef78a6f15fd9f09ad72513d2f2289432c11
RequiemG <995428887@qq.com>
2021-03-02 21:13:03 +0800

    setmeal图片redis完成版

--- health_parent/health_jobs/src/main/java/com/itheima/jobs/ClearImgJob.java
+++ health_parent/health_jobs/src/main/java/com/itheima/jobs/ClearImgJob.java
@@ -8,16 +8,15 @@ import redis.clients.jedis.JedisPool;
 import java.util.Set;
 
 public class ClearImgJob {
-
     @Autowired
     private JedisPool jedisPool;
-
     public void clearImg(){
         Set<String> set = jedisPool.getResource().sdiff(RedisConstant.SETMEAL_PIC_RESOURCES, RedisConstant.SETMEAL_PIC_DB_RESOURCES);
+        System.out.println(set);
         if (set!=null){
             for (String s : set) {
-                AliyunUtil.deleteFileFromAliYun(s);
                 jedisPool.getResource().srem(RedisConstant.SETMEAL_PIC_RESOURCES,s);
+                AliyunUtil.deleteFileFromAliYun(s);
                 System.out.println("自定义任务删除垃圾图片=>"+s);
             }
         }
